08091d8b71e6460f9c140aced81c17132aeb39d3
ornette coleman <ocoleman@ornettes-MacBook-Air.local>
2012-05-28 19:31:59 -0400

    moved the animation start positions out of the CSS and into the JS

--- bdp/js/vvCVR_anim.js
+++ bdp/js/vvCVR_anim.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
+	$('#vvCVR').css({opacity:0});
+	$('#cover').css({left:-700});
+	$('#credits').css({opacity:0});
 	setInterval(function(){
 		$('#vinyl').animate({rotate:'+=5deg'},0);
 	},45);
 		$('#vvCVR').animate({opacity:1},1000,function(){
 		$('#cover').delay(2000).animate({left:50},500).animate({left:0},200, function(){
-			$('#message2').delay(2000).animate({opacity:1},1000);
+			$('#credits').delay(2000).animate({opacity:1},1000);
 		});
 	});
 });
