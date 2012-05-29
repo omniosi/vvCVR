3c242887878a90b0701ac32748c12be699c18241
ornette coleman <ocoleman@ornettes-MacBook-Air.local>
2012-05-28 20:44:58 -0400

    final animation of background.

--- bdp/js/vvCVR_anim.js
+++ bdp/js/vvCVR_anim.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
 	$('#vvCVR').css({opacity:0});
+	$('#block').css({opacity:0});
 	$('#cover').css({left:-700});
 	$('#credits').css({opacity:0});
 	setInterval(function(){
 		$('#vinyl').animate({rotate:'+=5deg'},0);
 	},45);
-		$('#vvCVR').animate({opacity:1},1000,function(){
+	$('#vvCVR').animate({opacity:1},1000,function(){
+		$('#block').delay(2000).animate({opacity:1});
 		$('#cover').delay(2000).animate({left:50},500).animate({left:0},200, function(){
 			$('#credits').delay(2000).animate({opacity:1},1000);
 		});
 	});
+	/*$('#vvCVR').delay(2000).animate({background:'none'},500);*/
 });
