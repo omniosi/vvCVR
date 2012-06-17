d3b68cd04a30bde4c9b2a896f6949f8faf66c632
ornette coleman <ocoleman@ornettes-MacBook-Air.local>
2012-06-17 12:23:36 -0400

    changed rotation script to jQueryRotate
    the rotate-scale script was constantly updating the rotation value ad
    infinitum. this script rotates to 360 then goes back to 0.

--- bdp/js/vvCVR_anim.js
+++ bdp/js/vvCVR_anim.js
@@ -3,11 +3,21 @@ $(document).ready(function(){
 	$('#block').css({opacity:0});
 	$('#cover').css({left:-700});
 	$('#credits').css({opacity:0});
-	setInterval(function(){
-		$('#vinyl').animate({rotate:'+=5deg'},0);
-	},45);
+	var rotation = function (){
+    $("#vinyl").rotate({
+    	duration:2500,
+      angle:0, 
+      animateTo:360, 
+      callback: rotation,
+      easing: function (x,t,b,c,d){        // t: current time, b: begInnIng value, c: change In value, d: duration
+          return c*(t/d)+b;
+      }
+    });
+  }
+  rotation();
 	$('#vvCVR').animate({opacity:1},1000,function(){
 		$('#block').delay(2000).animate({opacity:1});
+		$('#intro').delay(2000).animate({opacity:0});
 		$('#cover').delay(2000).animate({left:50},500).animate({left:0},200, function(){
 			$('#credits').delay(2000).animate({opacity:1},1000);
 		});
